eb95b0ee2dae7329c5cc4fe4d6921267aa405fde
Witkowski <dev.witkowski@gmail.com>
2021-01-10 15:55:47 +0100

    working number checking

--- src/main/java/pl/sda/App.java
+++ src/main/java/pl/sda/App.java
@@ -8,30 +8,55 @@ import okhttp3.Response;
 import pl.sda.converters.GsonProvider;
 import pl.sda.model.GameType;
 import pl.sda.model.Games;
-import pl.sda.model.Order;
 import pl.sda.network.OkHttp;
 import pl.sda.network.RequestBuilder;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
 
 public class App {
 
     public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Prosze podaj gre ktora chcesz sprawdzic");
+        System.out.print("[ ");
+        for (GameType type : GameType.values()) {
+            System.out.print(type.getName() + " ");
+        }
+        System.out.print("]\n");
+
+        String gameTypeName = scanner.nextLine();
+        GameType gameType = GameType.getTypeByName(gameTypeName);
+        int[] userNumbers = new int[gameType.getMaxNumberCount()];
+        //Lotto
+        //1,3,6,9,13,25
+        int numCounter = 0;
+
+        do {
+            System.out.print("podaj liczbe nr " + (numCounter + 1) +": ");
+            userNumbers[numCounter] = scanner.nextInt();
+            numCounter++;
+        } while (numCounter < gameType.getMaxNumberCount());
+
+
         Gson gson = GsonProvider.SINGLETON.getGson();
         OkHttpClient client = OkHttp.INSTANCE.getClient();
 
-        Request request = new RequestBuilder(GameType.SZYBKIE_600)
-                .page(1)
-                .fetchCount(10)
-                .order(Order.ASC)
-                .build();
+        Request request = new RequestBuilder(gameType).build();
 
         Response response = client.newCall(request).execute();
         String stringresposne = response.body().string();
 
         Games dataFromBackend = gson.fromJson(stringresposne, Games.class);
+        List<Integer> wonNumbers = dataFromBackend.getItems().get(0).getResults().get(0).getNumbers();
 
-        System.out.println(dataFromBackend);
+        System.out.println("Gra: " + dataFromBackend.getItems().get(0).getGameType().getName());
+        System.out.println("Data losowania: " + dataFromBackend.getItems().get(0).getDrawDate().toString());
+        System.out.println(Arrays.toString(userNumbers));
+        System.out.println(wonNumbers);
+
     }
 
 }
--- src/main/java/pl/sda/model/GameType.java
+++ src/main/java/pl/sda/model/GameType.java
@@ -1,21 +1,27 @@
 package pl.sda.model;
 
 public enum GameType {
-    LOTTO("Lotto"), MINI_LOTTO("MiniLotto"), KASKADA("Kaskada"), SZYBKIE_600("Szybkie600");
+    LOTTO("Lotto", 6), MINI_LOTTO("MiniLotto", 5), KASKADA("Kaskada", 12), SZYBKIE_600("Szybkie600", 6);
 
     private final String name;
+    private final int maxNumberCount;
 
-    GameType(String name) {
+    GameType(String name, int maxNumberCount) {
         this.name = name;
+        this.maxNumberCount = maxNumberCount;
     }
 
     public String getName() {
         return name;
     }
 
+    public int getMaxNumberCount() {
+        return maxNumberCount;
+    }
+
     public static GameType getTypeByName(String searchingName) {
-        for (GameType type : GameType.values()){
-            if(type.getName().equals(searchingName)){
+        for (GameType type : GameType.values()) {
+            if (type.getName().equalsIgnoreCase(searchingName)) {
                 return type;
             }
         }
